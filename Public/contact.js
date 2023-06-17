9cae18111d637ca90c98b8642473cb9ab87eb37a
BOGUEH <blessingogueh1@gmail.com>
2023-06-17 23:43:18 +0100

    working with postman

---
+++ Public/contact.js
@@ -0,0 +1,24 @@
+
+
+contacts = [
+    {
+        "id": 1,
+        "name": "John Doe",
+        "email":"john@gmail.com",
+        "status": "active"
+    },
+    {
+        "id": 2,
+        "name": "bob Williams",
+        "email":"bob@gmail.com",
+        "status": "inactive"
+    },
+    {
+        "id": 3,
+        "name": "Jane Doe",
+        "email":"Jane@gmail.com",
+        "status": "active"
+    }
+];
+
+module.exports = contacts
--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const path = require('path');
+const contacts = require('./Public/contact')
+
 
 const app = express();
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000;
+
+
+
+app.get('/api/members', (req, res) =>{
+    res.json(contacts)
+});
+
+app.get('/api/members/:id', (req, res) =>{
+    res.json(contacts.filter(contacts=>contacts.id == req.params.id));
+});
+
+
 
 
 app.use(express.static(path.join(__dirname, 'public')));
